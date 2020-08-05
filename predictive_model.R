749d40ec629717b9b578e5723d83211cfc9d4e38
Clayton Ramsey <clayton.j.ramsey@gmail.com>
2020-08-05 19:14:11 -0400

    Added another model.

--- predictive_model.R
+++ predictive_model.R
@@ -1,7 +1,10 @@
+# I'm working on maintaining my skills by modeling this anime data and making predictive models.
+
 require(tidyverse)
 require(ggplot2)
 require(stringr)
 require(GGally)
+require(Metrics)
 
 ################## Data Cleaning and Preparing Variables ##########################
 
@@ -44,6 +47,8 @@ for(i in 1:length(genres[[1]])){
 
 # This is to make factor variables indicating the presence of each genre for each anime.
 rating_by_genre <- tibble(rating = anime$rating)
+# anime_name <- tibble(anime_name = anime$name)
+# rating_by_genre <- cbind(rating_by_genre, anime_name)
 for(i in 1:length(all_genres)){
   #create a variable for that genre:
   x <- integer(length(anime$genre))
@@ -95,11 +100,22 @@ means_table <- anime %>% group_by(genre_tag_number) %>% summarize(ave_rating = m
 mt <- ggplot(data = means_table, aes(x = genre_tag_number, y = ave_rating)) + geom_point() + geom_smooth()
 mt
 
+mt2 <- ggplot(data = anime, aes(x = genre_tag_number, y = rating)) + geom_point() + geom_smooth()
+mt2
+
 # Rating vs the log of members.
 mb <- ggplot(data = anime, aes(x = log(members), y = rating)) + geom_point()
+mb <- mb + geom_smooth()
 mb
 
 #################### Analyses ###########################
+
+# Models:
+# genre only
+# other show characteristics
+# hybrid
+
+# Dividing the data set into training and testing
 numrows <- length(anime$rating)
 training_indices <- sample(1:numrows, round(0.7*numrows), replace = FALSE)
 testing_indices <- setdiff(1:numrows, training_indices)
@@ -107,10 +123,8 @@ testing_indices <- setdiff(1:numrows, training_indices)
 # Starting with a high-dimension model estimating the rating from the presence or absense of genres.
 genremodel <- glm(rating ~ ., data = rating_by_genre[training_indices,])
 summary(genremodel)
-
 # Many of the genres show significance.
 genremodel <- step(genremodel)
-step(genremodel)
 summary(genremodel)
 
 #genre-based model predictions
@@ -118,8 +132,27 @@ ratings_predict <- predict(genremodel, rating_by_genre[testing_indices,])
 ratings_actual <- rating_by_genre$rating[testing_indices]
 
 # a model based on other characteristics
-model_2 <- glm(rating ~ genre_tag_number + I(genre_tag_number^2) + I(log(episodes)) + I(log(members)) + type,
+model_2 <- glm(rating ~ genre_tag_number + I(genre_tag_number^2) + I(log(episodes)) + type,
                data = anime[training_indices,])
 summary(model_2)
 ratings_predict_2 <- predict(model_2, anime[testing_indices,])
 rating_summary <- tibble(actual = ratings_actual, pred1 = ratings_predict, pred2 = ratings_predict_2)
+
+rating_summary
+
+# Now to see if we can improve the genre-based model by including variables from the second model.
+
+# This is to unify the data frames to have a source for prediction.
+anime_allvars <- cbind(rating_by_genre, anime)
+
+model_3 <- update(genremodel, . ~ . + genre_tag_number + I(genre_tag_number^2) + type,
+                  data = anime_allvars[training_indices,])
+ratings_predict_3 <- predict(model_3, anime_allvars[testing_indices,])
+
+rmse(ratings_actual, ratings_predict)
+rmse(ratings_actual, ratings_predict_2)
+rmse(ratings_actual, ratings_predict_3)
+
+mape(ratings_actual, ratings_predict)
+mape(ratings_actual, ratings_predict_2)
+mape(ratings_actual, ratings_predict_3)
